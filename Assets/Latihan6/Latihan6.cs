41b5c6de020a51845a2db6a32271f145906da3d3
robby.zr <88529263+robbyzr@users.noreply.github.com>
2021-08-20 15:10:26 +0700

    Add files via upload

---
+++ Assets/Latihan6/Latihan6.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan6 : MonoBehaviour
+{
+    public int[] intArray; //Deklarasi Array
+
+    void Start()
+    {
+        Debug.Log("Menampilkan Seluruh Array");
+        foreach (int a in intArray)
+        {
+            Debug.Log(a);
+        }
+        Debug.Log("Nilai Index ke 2 adalah " + intArray[2]);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
