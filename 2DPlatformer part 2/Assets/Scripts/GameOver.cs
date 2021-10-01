051bb2b4bcdec387c7a50fb1238e2b1dc98ded4e
robby.zr <88529263+robbyzr@users.noreply.github.com>
2021-10-01 18:29:28 +0700

    2D Platformer part 2 Done

---
+++ 2DPlatformer part 2/Assets/Scripts/GameOver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public Text txScore;
+    public Text txHighScore;
+    Text txSelamat;
+    int highscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("HS", 0);
+        if (Data.score > highscore)
+        {
+            highscore = Data.score;
+            PlayerPrefs.SetInt("HS", highscore);
+        }
+        else if (EnemyController.EnemyKilled == 3)
+        {
+            SceneManager.LoadScene("Congratulations");
+        }
+        txHighScore.text = "Highscores: " + highscore;
+        txScore.text = "Scores: " + Data.score;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Replay()
+    {
+        Data.score = 0;
+        EnemyController.EnemyKilled = 0;
+        SceneManager.LoadScene("Gameplay");
+    }
+}
